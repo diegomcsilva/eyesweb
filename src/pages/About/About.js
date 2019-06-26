6681d45f806b051b3df7604783a789a5c7b5b837
Diego <diego.silva@profite.com.br>
2019-06-26 02:34:18 -0300

    about basic

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 
-import styled from 'styled-components';
+const Title = styled.h1`
+    font-size: 48px;
+    line-height: 59px;
+    letter-spacing: 0.02em;
+`
 
-const BodyAbout = styled.div`
-    display: flex;
-    contain: ce;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    height: 100vh;
-    line-height: 38px;
+const Paragraph = styled.p`
+    font-size: 24px;
+    line-height: 28px;
+    margin-top: 20px;
+    font-family: 'Rubik', sans-serif;
 `
 
 class About extends Component {
@@ -23,14 +25,14 @@ class About extends Component {
                 
                 <main>
 
-                    <BodyAbout>
-                        <p>Prazer...</p>
-                        <p>MEU NOME É <strong>DIEGO MARCELO</strong> e sou desenvolvedor front end e muito curioso...</p>
-                        <p>Atuo como desenvolvedor front-end, sou formado em administração de empresas e pós-graduado em engenharia de software.</p>
-                        <p>Estudo sempre e me atualizo com tecnologias que abordem conceitos voltados para uma melhor performasse e usabilidade.</p> 
-                        <p>Desenvolver sites bem elaborados e organizados São a chave para um bom resultado.</p>
-                        <p>Aligidade é um conceito que deve ser visto como a mágica do negócio.</p>
-                    </BodyAbout>
+                    <Title>ABOUT EYESWEB</Title>
+
+                    <Paragraph>Prazer...
+                    <br /> MEU NOME É <strong>DIEGO MARCELO</strong> e sou desenvolvedor front end e muito curioso...
+                    <br />Atuo como desenvolvedor front-end, sou formado em administração de empresas e pós-graduado em engenharia de software.
+                    <br />Estudo sempre e me atualizo com tecnologias que abordem conceitos voltados para uma melhor performasse e usabilidade.
+                    <br />Desenvolver sites bem elaborados e organizados São a chave para um bom resultado.
+                    <br />Aligidade é um conceito que deve ser visto como a mágica do negócio.</Paragraph>
 
                 </main>
 
